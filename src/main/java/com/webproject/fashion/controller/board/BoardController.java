ca15aa7f2b28f64950b685cb613fef2b0d0497d8
kwangjin <kwangjin5468@naver.com>
2023-11-03 01:38:41 +0900

    Feat : 게시판 페이지 추가 (board-list.html, board-list.css)

---
+++ src/main/java/com/webproject/fashion/controller/board/BoardController.java
@@ -0,0 +1,14 @@
+package com.webproject.fashion.controller.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/list")
+    public String boardList() {
+        return "board/board-list";
+    }
+}
